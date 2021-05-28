dc0300b5a2a21e91a160425b6c6b3a24a4d8175e
d4xn <80895035+d4xn@users.noreply.github.com>
2021-05-28 18:28:47 +0200

    esx_euromillon_d4xn Version 0.1.0

---
+++ esx_euromillon_d4xn/euromillon.sql
@@ -0,0 +1,3 @@
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`, `price`, `weight`) VALUES
+	('boleto_euromillon', 'Boleto Euromillón', -1, 0, 1, 12, 1)
+;
---
+++ esx_euromillon_d4xn/server/server.lua
@@ -0,0 +1,19 @@
+ESX								= nil
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
+-- Compra de boletos
+RegisterServerEvent('esx_euromillon_d4xn:darBoletos')
+AddEventHandler('esx_euromillon_d4xn:darBoletos', function()
+    local _source = source
+    local xPlayer = ESX.GetPlayerFromId(_source)
+    if xPlayer.getMoney() >= 12 then
+
+        xPlayer.addInventoryItem('boleto_euromillon', 1)
+        xPlayer.removeMoney(12)
+    end
+    if xPlayer.getMoney() < 12 then
+
+        TriggerClientEvent('esx:showNotification', _source, 'No tienes dinero!')
+    end
+    
+end)
