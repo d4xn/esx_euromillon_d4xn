16066c26b5b812a00fd3075c777d424b95126a79
d4xn <80895035+d4xn@users.noreply.github.com>
2021-05-28 21:37:16 +0200

    Add files via upload

---
+++ esx_euromillon_d4xn/euromillon.sql
@@ -0,0 +1,3 @@
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`, `price`, `weight`) VALUES
+	('boleto_euromillon', 'Boleto Euromillon', -1, 0, 1, 12, 1)
+;
---
+++ esx_euromillon_d4xn/fxmanifest.lua
@@ -0,0 +1,14 @@
+-- Resource Metadata
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'd4xn' -- DanX
+description 'esx_euromillon_d4xn'
+version '0.1.0'
+
+-- What to run
+client_scripts {
+    'client/client.lua',
+    'config.lua'
+}
+server_script 'server/server.lua'
