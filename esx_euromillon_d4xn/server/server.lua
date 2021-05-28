5d5124de0dd57421ecd9542a46460deeda06456a
d4xn <80895035+d4xn@users.noreply.github.com>
2021-05-29 01:46:52 +0200

    Update server.lua

--- esx_euromillon_d4xn/server/server.lua
+++ esx_euromillon_d4xn/server/server.lua
@@ -2,18 +2,20 @@ ESX								= nil
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
 -- Compra de boletos
-RegisterServerEvent('esx_euromillon_d4xn:darBoletos')
-AddEventHandler('esx_euromillon_d4xn:darBoletos', function()
+RegisterServerEvent('esx_euromillon_d4xn:darBoletos_d4xn')
+AddEventHandler('esx_euromillon_d4xn:darBoletos_d4xn', function()
     local _source = source
     local xPlayer = ESX.GetPlayerFromId(_source)
-    if xPlayer.getMoney() >= 12 then
+    local coords = xPlayer.getCoords(true)
+    if #(coords-vector3(219.80, -859.85, 29.20)) <= 3 then
+        if xPlayer.getMoney() >= 12 then
 
-        xPlayer.addInventoryItem('boleto_euromillon', 1)
-        xPlayer.removeMoney(12)
-    end
-    if xPlayer.getMoney() < 12 then
+            xPlayer.addInventoryItem('boleto_euromillon', 1)
+            xPlayer.removeMoney(12)
 
-        TriggerClientEvent('esx:showNotification', _source, '~r~No ~w~tienes dinero!')
+        elseif xPlayer.getMoney() < 12 then
+
+            TriggerClientEvent('esx:showNotification', _source, '~r~No ~w~tienes dinero!')
+        end
     end
-    
 end)
