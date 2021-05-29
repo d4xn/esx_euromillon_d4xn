5db06184819a412247085b8a6ea88ac112263062
d4xn <80895035+d4xn@users.noreply.github.com>
2021-05-29 13:55:57 +0200

    Update config.lua

--- esx_euromillon_d4xn/config.lua
+++ esx_euromillon_d4xn/config.lua
@@ -1,7 +1,7 @@
 local blips = {
   -- Example {title="", colour=, id=, x=, y=, z=},
   -- Change the title if you speak other language
-  {title="Venta Euromillon", colour=7, id=280, x = 219.80, y = -859.85, z = 30.20}, -- x= 219.80, y= -859.85, z= 30.20
+  {title="Venta Euromillon", colour=7, id=280, x = 219.80, y = -859.85, z = 30.20}, -- Change the coordinates of the blip if you want
 
 }
 
