f6a35e50cfc1b97b24a0ab9ff382d3598d4cbd5c
d4xn <80895035+d4xn@users.noreply.github.com>
2021-05-29 12:06:15 +0200

    Update client.lua

--- esx_euromillon_d4xn/client/client.lua
+++ esx_euromillon_d4xn/client/client.lua
@@ -12,8 +12,8 @@ end)
 
 -- Venta Boleto NPC
 Citizen.CreateThread(function()
-    SpawnNPC('a_f_m_eastsa_01', vector4(219.80, -859.85, 29.20, 340.30)) --'a_f_m_eastsa_01' - 'a_c_shepherd'
-    SpawnNPC('a_c_shepherd', vector4(220.50, -860.15, 29.20, 340.30)) -- Dog
+    SpawnNPC('a_f_m_eastsa_01', vector4(219.80, -859.85, 29.20, 340.30)) -- Person (Vendedor/a Euromillon)
+    SpawnNPC('a_c_shepherd', vector4(220.50, -860.15, 29.20, 340.30)) -- Dog (comment this line for get lower cost and usage)
     while true do
         local _char = PlayerPedId()
         local _charPos = GetEntityCoords(_char)
