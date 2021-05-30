6399563f8a45b18cbe04665eac503cc6d9565a18
d4xn <80895035+d4xn@users.noreply.github.com>
2021-05-30 14:11:21 +0200

    Update euromillon.sql

--- esx_euromillon_d4xn/euromillon.sql
+++ esx_euromillon_d4xn/euromillon.sql
@@ -1,4 +1,4 @@
-INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`, `price`, `weight`) VALUES
-	('boleto_euromillon', 'Boleto Euromillon', -1, 0, 1, 12, 1)
+INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES
+	('boleto_euromillon', 'Boleto Euromillon', 1, 0, 1)
 ;
 	-- Change the label if you speak another language, and if u want change le limit or whatever
